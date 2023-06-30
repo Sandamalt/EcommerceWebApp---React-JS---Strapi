130efc151956c624fcafbb2101dd27bf773a772d
sandamalt <sandamal.live@gmail.com>
2023-06-30 15:01:07 +0530

    Footer Added & Complted

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            This is about. fsdhjkfdhjdjkvsjkvnjv sdvfsdvsvsfdvs
+            vsdvsdvsvsfdvsdfvsdfv
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            this is Contact dklsfjlaodjkfjlkajfckl vcfadfcdcfd
+            fdacdacadcadcfadcadc
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo"> THUNDER MAC</span>
+          <span className="copyright"> Copyright 2023 . All Rights </span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
