f8ddd7b4bb3424c967e9147cf6815ef3b0a993e6
sandamalt <sandamal.live@gmail.com>
2023-07-10 00:10:04 +0530

    Add data Fetch from db and imgs

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,41 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FeaturedProducts.scss";
 import Card from "../Card/Card";
+import axios from "axios";
+// require("dotenv").config();
 
 const FeaturedProducts = ({ type }) => {
-  const data = [
-    {
-      id: 1,
-      img: "https://images.pexels.com/photos/1710481/pexels-photo-1710481.jpeg?auto=compress&cs=tinysrgb&w=600",
-      img2: "https://images.pexels.com/photos/3026026/pexels-photo-3026026.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "iPhone",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 2,
-      img: "https://images.pexels.com/photos/3771084/pexels-photo-3771084.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "Tablet",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 3,
-      img: "https://images.pexels.com/photos/356055/pexels-photo-356055.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "Mac Book",
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 4,
-      img: "https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=600",
-      title: "Laptop",
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(
+          import.meta.env.VITE_API_URL + "/products?populate=*",
+          {
+            headers: {
+              Authorization: "bearer " + import.meta.env.VITE_API_TOKEN,
+            },
+          }
+        );
+        setData(res.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // console.log(data);
 
   return (
     <div className="featuredProducts">
@@ -58,3 +49,48 @@ const FeaturedProducts = ({ type }) => {
 };
 
 export default FeaturedProducts;
+
+//   const [products, setProducts] = useState([]);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get(
+//           process.env.REACT_APP_API_URL + "/products",
+//           {
+//             headers: {
+//               Authorization: "Bearer " + process.env.REACT_APP_API_TOKEN,
+//             },
+//           }
+//         );
+//         const products = response.data;
+//         setProducts(products);
+//         console.log(products);
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className="featuredProducts">
+//       <div className="top">
+//         <h1>{type} products</h1>
+//         <p>
+//           It is a device mainly used for a voice call. Presently technological
+//           advancements have made our life easy. Today, with the help of a mobile
+//           phone we can easily talk or video chat with anyone across the globe by
+//           just moving our fingers.
+//         </p>
+//       </div>
+//       <div className="bottom">
+//         {products.map((item) => (
+//           <Card item={item} key={item.id} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FeaturedProducts;
